1fe91204ff823d04d039906616fc8ce65de3978d
toneofrain <toneofrain@yonsei.ac.kr>
2022-08-20 03:16:11 +0900

    feat: ball 개수를 계산하는 countBalls 기능 추가

--- src/main/java/Baseball/Host.java
+++ src/main/java/Baseball/Host.java
@@ -1,44 +1,60 @@
 package Baseball;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 public class Host {
     private final static Random random = new Random();
     private final static int count = 3;
     private final static int min = 1;
     private final static int max = 9;
-    private final int[] numbers;
+    private final List<Integer> numbers;
     private int[] balls;
 
     private Host() {
         this.numbers = generateNumbers();
-        this.balls = new int[count];
     }
     private static int generate1to9() {
         return random.nextInt(max - min + 1) + min;
     }
-    private static int[] generateNumbers() {
+    private static List<Integer> generateNumbers() {
         Set<Integer> numberSet = new HashSet<>(count, 1);
         while (numberSet.size() < count) {
             numberSet.add(generate1to9());
         }
 
-        return numberSet.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
+        return new ArrayList<>(numberSet);
     }
 
-    public int[] storeBalls(String input) {
+    public void storeBalls(String input) {
         int[] store = new int[count];
 
         for (int i = 0; i < count; i++) {
             store[i] = input.charAt(i) - '0';
         }
 
-        return store;
+        balls = store;
     }
+
+    private int plusBallcount(int number) {
+        if (numbers.contains(number)) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    public int countBalls() {
+        int ballCount = 0;
+
+        for (int i = 0; i < count; i++) {
+            ballCount += plusBallcount(balls[i]);
+        }
+
+        return ballCount;
+    }
+
+
+
 
     public static void main(String[] args) {
 
