e935887400e5a374ab83a6aa9d9bf5112a4470c1
toneofrain <toneofrain@yonsei.ac.kr>
2022-08-19 22:30:35 +0900

    test: set size 테스트 구현

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,28 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setup() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void size() {
+        int actual = numbers.size();
+        assertThat(actual).isEqualTo(3);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -26,5 +27,26 @@ public class StringTest {
     void substring() {
         String actual = "(1,2)".substring(2,5);
         assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("인덱스를 초과하는 값을 넣었을 떄 예외")
+    void charAt() {
+        String str = "abc";
+        int index = 3;
+        assertThatThrownBy(() -> str.charAt(index))
+                .isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("%d", index);
+    }
+
+    @Test
+    @DisplayName("인덱스를 초과하는 값을 넣었을 떄 예외2")
+    void charAt2() {
+        String str = "abc";
+        int index = 3;
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+                .isThrownBy(() -> str.charAt(index))
+                .withMessageContaining("d", index);
     }
 }
